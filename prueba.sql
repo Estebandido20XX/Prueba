467eb8ec061d6ef2b71322972b26f2c9b7df2607
Esteban <51241959+Estebandido20XX@users.noreply.github.com>
2020-03-26 15:34:23 -0300

    Nuevo commit

--- prueba.sql
+++ prueba.sql
@@ -10,7 +10,17 @@ Edad int not null
 );
 
 Create table Empresa(
-NombreEmpresa Varchar(50) not null primary key,
+CodigoEmpresa int not null primary key,
+NombreEmpresa Varchar(50) not null,
 CodigoEmpleador int foreign key REFERENCES Empleador(CodigoEmpleador)
 );
 
+Insert into Empleador values(1,"Esteban","Sanchez",20);
+Insert into Empleador values(2,"Ignacio","Venegas",20);
+Insert into Empleador values(3,"Jorge","Sanchez",45);
+Insert into Empleador values(4,"Fernando","Ibarra",45);
+
+Insert into Empresa values(1,"Sanchez&Cia.",4);
+Insert into Empresa values(2,"IPSantiago",3);
+Insert into Empresa values(3,"SQL Inc.",2);
+Insert into Empresa values(4,"Java Ltda.",1);
